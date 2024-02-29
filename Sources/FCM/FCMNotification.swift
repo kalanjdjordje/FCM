65621e89d925c5357bcd3d7de846c1f52c55e38e
Kalanj Djordje <djordje@gaiali.com>
2024-02-29 22:58:17 +0100

    Added image to notification

--- Sources/FCM/FCMNotification.swift
+++ Sources/FCM/FCMNotification.swift
@@ -4,11 +4,11 @@ public struct FCMNotification: Codable {
     
     /// The notification's body text.
     var body: String
-    var image: String = ""
+    var image: String
     /// - parameters:
     ///     - title: The notification's title.
     ///     - body: The notification's body text.
-    public init(title: String, body: String, image: String = "") {
+    public init(title: String, body: String, image: String) {
         self.title = title
         self.body = body
         self.image = image
